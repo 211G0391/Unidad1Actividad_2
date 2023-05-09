c65510852c9afbb026e4449932a00d37acb1da34
Adrian <Adrian@DESKTOP-D4CMORG>
2023-05-08 20:09:47 -0500

    Agregar archivos de proyecto.

---
+++ Unidad1Actividad_2/ClaseMASTERMIND.cs
@@ -0,0 +1,80 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Unidad1Actividad_2
+{
+    public class ClaseMASTERMIND : INotifyPropertyChanged
+    {
+        private int[] valores = new int[4];
+
+        private int aciertos;
+        private int oportunidades;
+
+
+        public int Oportunidades
+        {
+            get { return oportunidades; }
+            set { oportunidades = value; }
+        }
+
+        public int Aciertos
+        {
+            get { return aciertos; }
+            set { aciertos = value; }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool JuegoIniciado { get; set; } = false;
+
+        public string Resultado { get; set; }
+        public void Iniciar()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                valores[i] = r.Next(0, 10);
+            }
+            oportunidades = 10;
+            aciertos = 0;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+
+        public void Verificar(int[] numeros)
+        {
+            for (int i = 0; i<valores.Length; i++)
+            {
+                if (numeros[i] == valores[i])
+                {
+                    aciertos++;
+                }
+            }
+            if (aciertos==4)
+            {
+                Resultado = "Felicidades, Acertaste";
+                JuegoIniciado = false;
+            }
+            else if (oportunidades==0)
+            {
+                Resultado = "Has fallado";
+                JuegoIniciado = false;
+            }
+        }
+        public ICommand IniciarComando { get; set; }
+        public ICommand VerificarComando { get; set; }
+
+        public ClaseMASTERMIND()
+        {
+            IniciarComando = new RelayCommand(Iniciar);
+            VerificarComando = new RelayCommand<int[]>(Verificar);
+
+        }
+
+    }
+}
